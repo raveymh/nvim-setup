b1c122c2085d6b0e46122a9e3db5abcd974e2293
raveymh <>
2024-10-16 18:59:16 +0800

    change hotkey for format buffer

--- init.lua
+++ init.lua
@@ -285,6 +285,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>bc', '::%bd|e#|bd#<CR>', { desc = '[C]lose other buffers' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -586,12 +587,12 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>m',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = 'For[m]at buffer',
       },
     },
     opts = {
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,28 +11,31 @@ return {
     harpoon:setup()
     -- REQUIRED
 
-    local conf = require('telescope.config').values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-        })
-        :find()
-    end
+    -- local conf = require('telescope.config').values
+    -- local function toggle_telescope(harpoon_files)
+    --   local file_paths = {}
+    --   for _, item in ipairs(harpoon_files.items) do
+    --     table.insert(file_paths, item.value)
+    --   end
+    --
+    --   require('telescope.pickers')
+    --     .new({}, {
+    --       prompt_title = 'Harpoon',
+    --       finder = require('telescope.finders').new_table {
+    --         results = file_paths,
+    --       },
+    --       previewer = conf.file_previewer {},
+    --       sorter = conf.generic_sorter {},
+    --     })
+    --     :find()
+    -- end
 
     vim.keymap.set('n', '<a-a>', function()
       harpoon:list():add()
     end, { desc = 'Add to harpoon' })
+    -- vim.keymap.set('n', '<leader>e', function()
+    --   toggle_telescope(harpoon:list())
+    -- end, { desc = 'Open harpoon window' })
 
     vim.keymap.set('n', '<a-1>', function()
       harpoon:list():select(1)
